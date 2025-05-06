789352fd997084bba768428bdd7b6db18463e611
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-05-06 09:21:51 -0400

    modified pages

--- BlazorTasker/obj/Debug/net8.0/BlazorTasker.AssemblyInfo.cs
+++ BlazorTasker/obj/Debug/net8.0/BlazorTasker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTasker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21fea570e5c5f1a8e09df72ed44a5c6b14ef07c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTasker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTasker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
