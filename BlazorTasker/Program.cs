3c4ffd6c103d2629fdf79b980f21c730f85b5664
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2025-05-07 08:27:51 -0400

    chore: reorganize componenets and add bottstrap icons

--- BlazorTasker/Program.cs
+++ BlazorTasker/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorTasker;
+using BlazorTasker.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
